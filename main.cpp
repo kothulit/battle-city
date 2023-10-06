e6f608a79da4256f8e1c7a97d5d7ce492504b5a8
kothulit <kothulit@gmail.com>
2023-10-06 17:37:26 +0500

    added second and third lines

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 int main()
 {
-	std::cout << "Yellow world" << std::end;
+	std::cout << "Yellow world" << std::endl;
+	std::cout << "Second line" << std::endl;
+	std::cout << "Third line" << std::endl;
 	return 0;
 }
